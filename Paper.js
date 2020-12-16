4570a869cf77d47e635db43c47b387445960e359
Pushpak1667 <73527668+Pushpak1667@users.noreply.github.com>
2020-12-16 22:24:44 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,34 @@
+class Paper{
+
+constructor(x,y,r){
+  var options = {
+   isStatic : false,
+   restitution : 0.3,
+   friction : 0.5,
+   density : 1.2  
+    }
+
+  this.body = Bodies.circle(x,y, r, options);
+  this.r = r;
+  World.add(world, this.body);
+
+}
+display()
+{
+var paperpos = this.body.position
+var angle = this.body.angle
+push()
+translate(paperpos.x,paperpos.y)
+rotate(angle)
+ellipseMode(RADIUS);
+strokeWeight(3);
+fill(255, 0, 255);
+
+ellipse(0,0,this.r);
+pop()
+
+
+
+}
+
+}
---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper;
+var ground;
+var box1,box2,box3;
+
+function setup() {
+	createCanvas(windowWidth,windowHeight);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paper = new Paper(200,500,50);
+
+ground = new Ground(windowWidth/2,windowHeight - 40,width,25);
+
+box1 = new Box(windowWidth - 90,windowHeight - 105,25,100);
+box2 = new Box(windowWidth - 412,windowHeight - 103,25,100);
+box3 = new Box(windowWidth - 250,windowHeight - 65,300,20);
+
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+paper.display();
+ground.display();
+
+box1.display();
+box2.display();
+box3.display();
+
+  drawSprites();
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:600 ,y:-600})
+	}
+}
